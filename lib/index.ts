9afdff80b88782a3635eb558472138140f25d642
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2026-02-11 15:09:43 +0200

    feat: re-export slab content and questions repositories in index file

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,5 @@
 // Re-export all database utilities
 export { default as pool, query, getClient, closePool } from "./db";
 export * from "./db-types";
+export * as slabContentRepo from "./repositories/slab-content";
+export * as questionsRepo from "./repositories/questions-asked";
