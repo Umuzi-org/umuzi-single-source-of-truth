e1e62cb9420c29256c725d47b44c9bfb31fd668f
Siyabonga Samson Lukhele <siyabonga.lukhele@umuzi.org>
2026-02-13 17:12:47 +0200

    feat: re-export content ingestion utilities and types for improved accessibility

--- lib/index.ts
+++ lib/index.ts
@@ -3,3 +3,9 @@ export { default as pool, query, getClient, closePool } from "./db";
 export * from "./db-types";
 export * as slabContentRepo from "./repositories/slab-content";
 export * as questionsRepo from "./repositories/questions-asked";
+
+// Content ingestion utilities
+export { loadAllDocuments } from "./content-reader";
+export type { MarkdownDocument } from "./content-reader";
+export { chunkDocument, chunkAllDocuments } from "./chunker";
+export type { ContentChunk } from "./chunker";
